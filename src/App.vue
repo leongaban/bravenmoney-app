e13f4bd09c37fa05ebb9a6734555001f30e2ff37
Leon Gaban <leongaban@gmail.com>
2024-08-12 08:22:57 -0500

    added Cascadia Code font family

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@ import CryptoTable from './components/CryptoTable.vue'
 </script>
 
 <template>
-  <main class="main montserrat-900">
+  <main class="main">
     <h1>BraveMoney</h1>
-    <h3 class="montserrat-100">An app to track your modern investing life</h3>
+    <h3>An app to track your modern investing life</h3>
+
+    $59,867.23
 
     <section>
       <CryptoTable />
